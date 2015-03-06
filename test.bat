5330368382ebd9f4e9dc81545cc5370e6b30f2d1
ikscoder <igorekiks@gmail.com>
2015-03-06 14:36:23 +0700

    еще 10 различных тестов
    Signed-off-by: ikscoder <igorekiks@gmail.com>

---
+++ test.bat
@@ -0,0 +1,12 @@
+DEL c:\1\test.log
+c:\1\tp.lab1.exe >> test.log
+c:\1\tp.lab1.exe troll >> test.log
+c:\1\tp.lab1.exe DEL >> test.log
+
+c:\1\tp.lab1.exe g c:\1\t.exe >> test.log
+c:\1\tp.lab1.exe c c:\1\t.exe c:\1\t2.exe >> test.log
+c:\1\tp.lab1.exe d c:\1\t.exe >> test.log
+c:\1\tp.lab1.exe r c:\1\t2.exe c:\1\t3.exe>> test.log
+
+
+

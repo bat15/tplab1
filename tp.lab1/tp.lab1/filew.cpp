11e92923067f9764ea3336a3918063906ea89a93
ikscoder <igorekiks@gmail.com>
2015-03-07 17:15:16 +0700

    17:14 07.03.15
    Signed-off-by: ikscoder <igorekiks@gmail.com>

--- tp.lab1/tp.lab1/filew.cpp
+++ tp.lab1/tp.lab1/filew.cpp
@@ -1,67 +1,50 @@
 #define _CRT_SECURE_NO_WARNINGS 1
-#include "tp.lab1.h"
+#include "argsParse.h"
+#include "filew.h"
 using namespace std;
 
 void filew::fcreate()
 {
-	if (fname1 == "" )
-	{
-		cout << "b)Error! Empty file name! Enter file name!" << endl;
-		return;
-	}
-	else
-	{
-		char *temp2 = new char[strlen(fname1)];
-		strcpy(temp2, fname1);
-		int i = strlen(temp2) - 1;
-		while (temp2[i] != '\\')
+		char *temp = new char[strlen(fname1)+1];
+		strcpy(temp, fname1);
+		int i = strlen(temp) - 1;
+		while (temp[i] != '\\')
 		{
 			i--;
 		}
 		i++;
-		temp2[i] = '\0';
-		if (_access(fname1, 0)&&!_access(temp2,0))
+		temp[i] = '\0';
+		if (_access(fname1, 0)&&!_access(temp,0))
 		{
-			ofstream desc(fname1); cout << "z)Successful" << endl;
+			ofstream desc(fname1);
 		}
 		else
-			cout << "g)cant create file" << endl;
-	}
+			cout << "Cant create file" << endl;
+		delete temp;
 }
 void filew::fdelete()
 {
-	if (fname1 == "")
-	{
-		cout << "b)Error! Empty file name! Enter file name!" << endl;
-		return;
-	}
-	else
-	{
-		char *temp = new char[strlen(fname1) + 5];
-		strcpy(temp,"DEL ");
+		char *temp = new char[strlen(fname1) + 7];
+		strcpy(temp,"DEL \"");
 		strcat(temp, fname1);
+		strcat(temp,"\"");
 		if (!_access(fname1, 0))
 		{
-			system(temp); cout << "z)Successful" << endl;
+			if (system(temp)!=0)	
+			cout << "unsuccessful system calling" << endl;
 		}
 		else
-			cout << "g)file not found" << endl;
-	}
+			cout << "file not found" << endl;
+		delete temp;
 }
 void filew::fcopy()
 {
-	if (fname1 == "" || fname2 == "")
-	{
-		cout << "b)Error! Empty file name! Enter file name!" << endl;
-		return;
-	}
-	else
-	{
-		char *temp = new char[strlen(fname1) + strlen(fname2) + 15];
-		strcpy(temp,"COPY ");
+		char *temp = new char[strlen(fname1) + strlen(fname2) + 19];
+		strcpy(temp,"COPY \"");
 		strcat(temp, fname1);
-		strcat(temp, " ");
+		strcat(temp, "\" \"");
 		strcat(temp,fname2);
+		strcat(temp, "\"");
 		char *temp2 = new char[strlen(fname1)];
 		strcpy(temp2, fname1);
 		int i = strlen(temp2) - 1;
@@ -73,46 +56,59 @@ void filew::fcopy()
 		temp2[i] = '\0';
 		if (_access(fname2, 0) && !_access(fname1, 0) && !_access(temp2, 0))
 		{
-			system(temp); cout << "z)Successful" << endl;
+			system(temp);
 		}
 		else
-			cout << "g)cant copy file or first file not found" << endl; 
-		
-	}
+			cout << "cant copy file or first file not found" << endl; 
+		delete temp, temp2;
 }
 void filew::frename()
 {
-	if (fname1 == ""||fname2=="")
-	{
-		cout << "b)Error! Empty file name!Enter file name!" << endl;
-		return;
-	}
-	else
-	{
 		if (rename(fname1, fname2) != 0)
-		{
-			cout << "c)Rename error" << endl; return;
-		}
-		else cout << "z)Successful" << endl;
-	}
+			cout << "rename error" << endl; return;
 }
+
+bool filew::fsearch(string filePath){
+		bool isExist = false;
+		char *repMes;
+		ifstream fin(filePath.c_str());
+		if (fin.is_open())isExist = true;
+		if (isExist == true) repMes=" YES_FILE_EXISTS";
+		else repMes = " FILE_NOT_FOUND";
+		cout << "\n WAY TO SEARCH " << filePath.c_str() << "\t";
+		cout << "\n IS FILE EXIST? => " << repMes << "\n";
+		fin.close();
+		return isExist;
+}
+
 void filew::getproc(char *fproc,char *str1,char *str2,filew *file,int argentum)
 {
 	fname1 = str1;
 	fname2 = str2;
 	char proc[6]; proc[0] = 'e';
 	*proc = *fproc;
-	switch (proc[0])
+
+	argsParsing *argsMASS = new argsParsing;
+
+	argsMASS->getCMNDArg(fproc);
+	
+	switch (argsMASS->CMNDarg)
 	{
-	case 'G':{if (argentum < 3){ cout << "d)argument error" << endl; return; } file->fcreate(); return; }
-	case 'D':{if (argentum < 3){ cout << "d)argument error" << endl; return; }file->fdelete(); return; }
-	case 'C':{if (argentum < 4){ cout << "d)argument error" << endl; return; }file->fcopy(); return; }
-	case 'R':{if (argentum < 4){ cout << "d)argument error" << endl; return; }file->frename(); return; }
-	case 'g':{if (argentum < 3){ cout << "d)argument error" << endl; return; } file->fcreate(); return; }
-	case 'd':{if (argentum < 3){ cout << "d)argument error" << endl; return; }file->fdelete(); return; }
-	case 'c':{if (argentum < 4){ cout << "d)argument error" << endl; return; }file->fcopy(); return; }
-	case 'r':{if (argentum < 4){ cout << "d)argument error" << endl; return; }file->frename(); return; }
-	case 'e':cout << "e)command was not given" << endl; return;
-	default: cout << "f)404 illegal command not found" << endl; return;
+	case 'G':{if (argentum < 3){ cout << "second argument error" << endl; break; } file->fcreate(); break; }
+	case 'D':{if (argentum < 3){ cout << "second argument error" << endl; break; }file->fdelete(); break; }
+	case 'C':{if (argentum < 4){ cout << "second argument error" << endl; break; }file->fcopy(); break; }
+	case 'R':{if (argentum < 4){ cout << "second argument error" << endl; break; }file->frename(); break; }
+
+	case 'N': {cout << "command was not given" << endl; break; }
+	case 'E': {cout << "wrong command was given" << endl; break; }
+	case 'A': {cout << "______________________________________________________\n\n"; break; }
+
+	case 'S':{if (argentum < 3){ cout << "second argument error" << endl; break; }file->fsearch(str1); break; }
+	default: {cout << "illegal command not found" << endl; break; }
 	}
+	delete argsMASS;
+	
 }
+
+
+
---
+++ tp.lab1/tp.lab1/filew.h
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <fstream>
+#include "io.h"
+using namespace std;
+class filew
+{
+private:
+	char *fname1,*fname2;
+protected:
+	void fcreate();
+	void fdelete();
+	void fcopy();
+	void frename();
+	bool fsearch(std::string);
+
+public:
+	filew(){}
+	~filew(){}
+	void getproc(char *,char *,char*,filew *,int);
+};
